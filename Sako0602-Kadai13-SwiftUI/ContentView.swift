fef0c07f84fa1744c3a047255e8100f52517ec49
sasasan03 <sako.hiro62@gmail.com>
2023-01-17 18:46:09 +0900

    FB修正

--- Sako0602-Kadai13-SwiftUI/ContentView.swift
+++ Sako0602-Kadai13-SwiftUI/ContentView.swift
@@ -8,32 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
-
-   private let fruits = [
-        "りんご": false,
-        "みかん": true,
-        "バナナ": false,
-        "パイナップル": true,
+    
+    private let fruits = [
+        Item(name: "りんご", isChecked: false),
+        Item(name: "みかん", isChecked: true),
+        Item(name: "なし", isChecked: false),
+        Item(name: "パイナップル", isChecked: true)
     ]
-
+    
     var body: some View {
         List {
-            ForEach(fruits.sorted{ $0.key < $1.key}, id: \.key) { element in
-                
+            ForEach(fruits, id: \.id) { fruit in
                 HStack {
-                    Image(
-                        systemName: element.value
+                    Image(systemName: fruit.isChecked
                           ? "checkmark"
                           : ""
                     )
-                    
-                    Text(element.key)
-                    
+                    .frame(width: 30, height: 30)
+                    Text(fruit.name)
                 }
             }
         }
     }
 }
+struct Item {
+    let id = UUID()
+    let name: String
+    let isChecked: Bool
+    
+}
+    
+/*
+ private let fruits = [
+      "りんご": false,
+      "みかん": true,
+      "バナナ": false,
+      "パイナップル": true,
+  ]
+
+  var body: some View {
+      List {
+          ForEach(fruits.sorted{ $0.key < $1.key}, id: \.key) { element in
+              
+              HStack {
+                  Image(
+                      systemName: element.value
+                        ? "checkmark"
+                        : ""
+                  )
+                  
+                  Text(element.key)
+                  
+              }
+          }
+      }
+  }
+}
+ */
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
